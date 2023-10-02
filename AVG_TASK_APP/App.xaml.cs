b0a6e77548c6468ab276e16d7d8a67739b9307d1
ntd1683 <ntdvlog1683@gmail.com>
2023-10-02 21:00:29 +0700

    Init

--- AVG_TASK_APP/App.xaml.cs
+++ AVG_TASK_APP/App.xaml.cs
@@ -16,8 +16,8 @@ namespace AVG_TASK_APP
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            HomeView homeView = new HomeView();
-            homeView.Show();
+            CreateWorkspaceView createWorkspaceView = new CreateWorkspaceView();
+            createWorkspaceView.Show();
         }
     }
 }
---
+++ AVG_TASK_APP/Views/CreateWorkspaceView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AVG_TASK_APP.Views
+{
+    /// <summary>
+    /// Interaction logic for CreateWorkspaceView.xaml
+    /// </summary>
+    public partial class CreateWorkspaceView : Window
+    {
+        public CreateWorkspaceView()
+        {
+            InitializeComponent();
+            txtDateStart.SelectedDate = DateTime.Today;
+        }
+
+        private void btnGenerateCode_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            string code = "AVG_" + rnd.Next();
+
+            txtCode.Text = code;
+        }
+
+        private void checkInput()
+        {
+
+        }
+
+        private void btnContinue_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
