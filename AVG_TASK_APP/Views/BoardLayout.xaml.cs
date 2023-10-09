03851186cac9f63f46862186c1736687e9b8d714
Huynguyen <nguyenmaibaohuy2003@gmail.com>
2023-10-09 13:06:30 +0700

    part1

---
+++ AVG_TASK_APP/Views/BoardLayout.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AVG_TASK_APP.Views
+{
+    /// <summary>
+    /// Interaction logic for BoardLayout.xaml
+    /// </summary>
+    public partial class BoardLayout : Window
+    {
+        public BoardLayout()
+        {
+            InitializeComponent();
+        }
+    }
+}
