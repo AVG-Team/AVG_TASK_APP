894dbb7b0c66e92a750e001b9bd075ca3bcd69e0
Huynguyen <nguyenmaibaohuy2003@gmail.com>
2023-10-16 10:44:57 +0700

    update

---
+++ AVG_TASK_APP/CustomControls/ManageTaskUserControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AVG_TASK_APP.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for ManageTaskUserControl.xaml
+    /// </summary>
+    public partial class ManageTaskUserControl : UserControl
+    {
+        public ManageTaskUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void StackPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
