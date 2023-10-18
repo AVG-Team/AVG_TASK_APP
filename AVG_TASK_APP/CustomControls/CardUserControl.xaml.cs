13b3b65f84b82b49cfa75fa49ff10a11e57d06ed
Huynguyen <nguyenmaibaohuy2003@gmail.com>
2023-10-18 13:40:02 +0700

    update

--- AVG_TASK_APP/CustomControls/CardUserControl.xaml.cs
+++ AVG_TASK_APP/CustomControls/CardUserControl.xaml.cs
@@ -1,9 +1,5 @@
-﻿using AVG_TASK_APP.Models;
-using C1.WPF.Core;
-using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +7,6 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -23,120 +18,13 @@ namespace AVG_TASK_APP.CustomControls
     /// <summary>
     /// Interaction logic for CardUserControl.xaml
     /// </summary>
-    public partial class CardUserControl : System.Windows.Controls.UserControl
+    public partial class CardUserControl : UserControl
     {
-        private Point _dragStartPoint;
-
-        private T FindVisualParent<T>(DependencyObject child)
-            where T : DependencyObject
-        {
-            var parentObject = VisualTreeHelper.GetParent(child);
-            if (parentObject == null)
-                return null;
-            T parent = parentObject as T;
-            if (parent != null)
-                return parent;
-            return FindVisualParent<T>(parentObject);
-        }
-
-        private IList<Person> _items = new ObservableCollection<Person>();
+        public ListBox lb { get { return _list; } }
 
         public CardUserControl()
         {
             InitializeComponent();
-            _items.Add(new Person() { Name = "John Doe1" });
-            _items.Add(new Person() { Name = "John Doe2" });
-
-            _items.Add(new Person() { Name = "John Doe3" });
-            _items.Add(new Person() { Name = "John Doe4" });
-            _items.Add(new Person() { Name = "John Doe5" });
-
-
-
-            listBox.DisplayMemberPath = "Name";
-            listBox.ItemsSource = _items;
-
-            listBox.PreviewMouseMove += ListBox_PreviewMouseMove;
-
-            var style = new Style(typeof(ListBoxItem));
-            style.Setters.Add(new Setter(ListBoxItem.AllowDropProperty, true));
-            style.Setters.Add(
-                new EventSetter(
-                    ListBoxItem.PreviewMouseLeftButtonDownEvent,
-                    new MouseButtonEventHandler(ListBoxItem_PreviewMouseLeftButtonDown)));
-            style.Setters.Add(
-                    new EventSetter(
-                        ListBoxItem.DropEvent,
-                        new System.Windows.DragEventHandler(ListBoxItem_Drop)));
-            listBox.ItemContainerStyle = style;
-        }
-
-        private void ListBox_PreviewMouseMove(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-
-            Point point = e.GetPosition(null);
-            Vector diff = _dragStartPoint - point;
-            if (e.LeftButton == MouseButtonState.Pressed &&
-                (Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
-                    Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance))
-            {
-                var lb = sender as System.Windows.Controls.ListBox;
-                var lbi = FindVisualParent<ListBoxItem>(((DependencyObject)e.OriginalSource));
-                if (lbi != null)
-                {
-                    DragDrop.DoDragDrop(lbi, lbi.DataContext, System.Windows.DragDropEffects.Move);
-                }
-            }
-        }
-        private void ListBoxItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            _dragStartPoint = e.GetPosition(null);
-        }
-
-        private void ListBoxItem_Drop(object sender, System.Windows.DragEventArgs e)
-        {
-            if (sender is ListBoxItem)
-            {
-                var source = e.Data.GetData(typeof(Person)) as Person;
-                var target = ((ListBoxItem)(sender)).DataContext as Person;
-
-
-
-                int sourceIndex = listBox.Items.IndexOf(source);
-                int targetIndex = listBox.Items.IndexOf(target);
-
-
-                Move(source, sourceIndex, targetIndex);
-            }
-        }
-
-        private void Move(Person source, int sourceIndex, int targetIndex)
-        {
-            if (sourceIndex < targetIndex)
-            {
-                _items.Insert(targetIndex + 1, source);
-                /*_items.RemoveAt(sourceIndex);*/
-            }
-            else
-            {
-                int removeIndex = sourceIndex + 1;
-                if (_items.Count + 1 > removeIndex)
-                {
-                    _items.Insert(targetIndex, source);
-                    _items.RemoveAt(removeIndex);
-                }
-            }
-        }
-
-
-        private void personElement_MouseEnter(object sender, MouseButtonEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void _list_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-
         }
     }
 }
--- AVG_TASK_APP/CustomControls/ManageTaskUserControl.xaml.cs
+++ AVG_TASK_APP/CustomControls/ManageTaskUserControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using C1.WPF.Core;
+﻿using AVG_TASK_APP.Models;
+using C1.WPF.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,33 +22,109 @@ namespace AVG_TASK_APP.CustomControls
     /// </summary>
     public partial class ManageTaskUserControl : UserControl
     {
-
+        private List<UserControl> _listCard = new List<UserControl>();
+        private List<ListBox> listBoxes = new List<ListBox>();
+        private C1DragDropManager _dd;
 
         public ManageTaskUserControl()
         {
-            try
-            {
-                InitializeComponent();
+            InitializeComponent();
 
-                for (int i = 1; i <= 5; i++)
+            _dd = new C1DragDropManager();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                CardUserControl cardUserControl = new CardUserControl();
+                areaCard.Children.Add(cardUserControl);
+                _listCard.Add(cardUserControl);
+            }
+            foreach (CardUserControl card in _listCard)
+            {
+                _dd.RegisterDropTarget(card.lb, true);
+                listBoxes.Add(card.lb);
+            }
+
+            foreach (ListBox lb in listBoxes)
+            {
+                foreach (Person p in Person.Generate(5))
                 {
-                    addCard(i);
+                    Button button = new Button();
+
+
+                    var personElement = new ContentPresenter();
+                    personElement.Content = p.Name;
+                    personElement.MouseLeftButtonUp += personElement_MouseEnter;
+                    personElement.ContentTemplate = (DataTemplate)Resources["listBoxItem"];
+                    lb.Items.Add(personElement);
+
+                    _dd.RegisterDragSource(personElement, DragDropEffect.Move, ModifierKeys.None);
+                    _dd.RegisterDragSource(button, DragDropEffect.Move, ModifierKeys.None);
+                    _dd.DragThreshold = 5;
+
+                    personElement.MouseDown += (s, e) =>
+                    {
+                        e.Handled = true;
+                    };
                 }
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+
+            _dd.DragDrop += _dd_DragDrop;
+
 
 
         }
 
-
-        private void addCard(int pos)
+        private void Button_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            CardUserControl cardUserControl = new CardUserControl();
+            throw new NotImplementedException();
+        }
 
-            areaCard.Children.Add(cardUserControl);
+        private void _dd_DragDrop(object source, DragDropEventArgs e)
+        {
+            // get object being dragged
+            UIElement sourceElement = e.DragSource;
+            // get source parent, target listboxes
+            foreach (ListBox lb in listBoxes)
+            {
+                if (lb.Items.Contains(sourceElement))
+                {
+                    ListBox sourceParent = lb;
+                    ListBox target = e.DropTarget as ListBox;
+                    // get index into target
+                    int index = GetDropIndex(e, target);
+                    // adjust index
+                    if (sourceParent == target)
+                    {
+                        int sourceIndex = sourceParent.Items.IndexOf(sourceElement);
+                        if (index > sourceParent.Items.IndexOf(sourceElement))
+                        {
+                            index--;
+                        }
+                        if (index == sourceIndex)
+                            return;
+                    }
+                    // remove from original position, insert into new position
+                    sourceParent.Items.Remove(sourceElement);
+                    target.Items.Insert(index, sourceElement);
+                }
+            }
+        }
+
+        private int GetDropIndex(DragDropEventArgs e, ListBox? target)
+        {
+            int index = 0;
+            foreach (UIElement child in target.Items)
+            {
+                Point p = e.GetPosition(child);
+                if (p.Y - child.DesiredSize.Height / 2 < 0) break;
+                index++;
+            }
+            return index;
+        }
+
+        private void personElement_MouseEnter(object sender, MouseButtonEventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         private void StackPanel_MouseDown(object sender, MouseButtonEventArgs e)
