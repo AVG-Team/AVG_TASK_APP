bc3a78457a8c74d774af7dee619c04ace1426051
ntd1683 <ntdvlog1683@gmail.com>
2023-10-28 10:05:29 +0700

    Recentlly in Home

--- AVG_TASK_APP/Repositories/TableRepository.cs
+++ AVG_TASK_APP/Repositories/TableRepository.cs
@@ -5,7 +5,9 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AVG_TASK_APP.Repositories
@@ -38,6 +40,27 @@ namespace AVG_TASK_APP.Repositories
                 return dbContext.Tables.OrderBy(s => s.Created_At).ToList();
         }
 
+        public IEnumerable<Table> GetAllForUser(string sort = "desc")
+        {
+            var identity = Thread.CurrentPrincipal.Identity as ClaimsIdentity;
+            if (identity == null)
+            {
+                return null;
+            }
+
+            int id = int.Parse(identity.Claims.FirstOrDefault(s => s.Type == "Id").Value);
+            var tables = dbContext.UserTables
+                                .Where(s => s.Id_User == id)
+                                .Select(s => s.Table);
+
+            if (sort.Equals("desc"))
+            {
+                return tables.Where(s => s.Deleted_At == null).OrderByDescending(s => s.Created_At).ToList();
+            }
+
+            return tables.Where(s => s.Deleted_At == null).OrderBy(s => s.Created_At).ToList();
+        }
+
         public IEnumerable<Table> GetAllForWorkspace(int idWorkspace, string sort = "desc")
         {
             if (sort.Equals("desc"))
@@ -49,6 +72,20 @@ namespace AVG_TASK_APP.Repositories
         public Table GetById(int idTable)
         {
             return dbContext.Tables.Where(s => s.Id == idTable).FirstOrDefault();
+        }
+
+        public Workspace GetWorkspace(int idTable)
+        {
+            int idWorkspace = dbContext.Tables.FirstOrDefault(s => s.Id == idTable).Id_Workspace;
+            return dbContext.Workspaces.FirstOrDefault(s => s.Id == idWorkspace);
+        }
+
+        public int GetRole(int idTable)
+        {
+            var identity = Thread.CurrentPrincipal.Identity as ClaimsIdentity;
+            int id = int.Parse(identity.Claims.FirstOrDefault(s => s.Type == "Id").Value);
+
+            return dbContext.UserTables.FirstOrDefault(s => s.Id_Table == idTable && s.Id_User == id).Role;
         }
 
         public void Remove(Table table)
